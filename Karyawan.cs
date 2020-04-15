4ae785ce00dc3c850ce7879d0d25c5975c3be5f7
leonmana <leonel.len9331@gmail.com>
2020-04-15 14:53:19 +0700

    data gaji bulanan

---
+++ Karyawan.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrogramman2907
+{
+   public class Karyawan
+    {
+        public int No { get; set; }
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+
+        public Karyawan(int No, int Nik, string Nama, int GajiBulanan)
+        {
+            this.No = No;
+            this.Nik = Nik;
+            this.Nama = Nama;
+            
+            if (GajiBulanan<0)
+            {
+                this.GajiBulanan = 0;
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+            }
+        }
+       
+        public void Data()
+        {
+            Console.WriteLine("{0}\t{01}\t {2}\t {3}", No, Nik, Nama, GajiBulanan);           
+        }
+        public void Naikgaji()
+        {
+            double tmp = 0;
+            tmp = 0.1 * GajiBulanan;
+            GajiBulanan += Convert.ToInt32(tmp);
+            Console.WriteLine("{0}\t{1}\t {2}\t {3}", No, Nik, Nama, GajiBulanan);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ResponsiPemrogramman2907
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan saya = new Karyawan(01, 91760459, "Leon", 700000);
+            Karyawan kamu = new Karyawan(02, 91824580, "Villa", 500000);
+
+            Console.WriteLine("No \tNik \t \t Nama \t Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            saya.Data();
+            kamu.Data();
+
+            Console.WriteLine("\n\n Selamat... gaji naik sebesar 10%\n\n");
+
+            Console.WriteLine("No \t Nik \t \t Nama \t Gaji Bulanan");
+            Console.WriteLine("--------------------------------------");
+            saya.Naikgaji();
+            kamu.Naikgaji();
+
+            Console.ReadKey();
+        }
+    }
+}
+
